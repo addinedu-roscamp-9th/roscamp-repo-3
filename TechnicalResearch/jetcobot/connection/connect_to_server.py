a248ace03716b35b39c93e6914ef173cba5cc11f
김찬희 <blackeagle10@icloud.com>
2026-01-30 16:51:23 +0900

    update(jetcoboto): connect and send data to server

--- TechnicalResearch/jetcobot/connection/connect_to_server.py
+++ TechnicalResearch/jetcobot/connection/connect_to_server.py
@@ -1,17 +1,28 @@
 import json
+from dataclasses import dataclass
 
 import requests
 
 with open("./config.json", encoding="UTF-8") as file:
     config = json.load(file)
 
-
 SERVER_URL = config["server_url"]
 
 
+@dataclass
+class JetcobotData:
+    robot_id: int
+    status: str
+
+
 def connect_to_server():
+    # TODO: refactor ardcode name
+    data = JetcobotData(robot_id=51, status="idle")
+
     try:
-        response = requests.post(SERVER_URL + "/jetcobot", timeout=5)
+        response = requests.post(
+            f"{SERVER_URL}/jetcobot", json=data.__dict__, timeout=5  # convert to dict
+        )
         print("status:", response.status_code)
         print("body:", response.text)
     except requests.RequestException as e:
