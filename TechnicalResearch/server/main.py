72f95859f5f608e31031c9b09a7debb3f6bf60f4
김찬희 <blackeagle10@icloud.com>
2026-02-04 13:12:14 +0900

    refactor(server): .env config.py run.py into main.py
    just run main.py

--- TechnicalResearch/server/main.py
+++ TechnicalResearch/server/main.py
@@ -1,8 +1,19 @@
-from app.routers import jetcobot, pinky, gui
+import uvicorn
 from fastapi import FastAPI
+
+from app.routers import gui, jetcobot, pinky
+
+HOST="0.0.0.0"
+PORT=8000
+DEBUG=True
 
 app = FastAPI(title="Roboto Server", redirect_slashes=False)
 
 app.include_router(jetcobot.router, prefix="/jetcobot", tags=["jetcobot"])
 app.include_router(pinky.router, prefix="/pinky", tags=["pinky"])
 app.include_router(gui.router, prefix="/gui", tags=["gui"])
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=DEBUG)
--- TechnicalResearch/server/run.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-from config import HOST, PORT, DEBUG
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host=HOST, port=PORT, reload=DEBUG)
