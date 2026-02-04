5b40503b0c1cfd35329a06672584717863e60b55
김찬희 <blackeagle10@icloud.com>
2026-02-04 17:26:46 +0900

    feat(server): return pos to jetcobot

--- TechnicalResearch/server/app/services/jetcobot_service.py
+++ TechnicalResearch/server/app/services/jetcobot_service.py
@@ -1,9 +1,24 @@
+from app.models.postures_model import Postures
 from app.models.robots_model import RobotsData
 
 
 def test_connection(data: RobotsData):
-    print(f'{data.robot_id}')
-    print(f'{data.namespace}')
-    print(f'{data.robot_type}')
-    print(f'{data.robot_name}')
-    return "Hello from jetcobot_service.py"
+    print(f"{data.robot_id}")
+    print(f"{data.namespace}")
+    print(f"{data.robot_type}")
+    print(f"{data.robot_name}")
+
+    pos = Postures(
+        pos_id="foo",
+        pos_name="bar",
+        j1=0.0,
+        j2=0.0,
+        j3=0.0,
+        j4=0.0,
+        j5=0.0,
+        j6=0.0,
+        angle=-135, # camera to bottom
+        gap=100,  # completely open
+    )
+
+    return pos.model_dump_json()
