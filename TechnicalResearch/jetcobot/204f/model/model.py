842c6a48ec9ea3edef4e48bf5283742a49e9943c
김찬희 <blackeagle10@icloud.com>
2026-02-04 16:40:27 +0900

    add(jetcoboto): PosturesData class

--- TechnicalResearch/jetcobot/204f/model/model.py
+++ TechnicalResearch/jetcobot/204f/model/model.py
@@ -6,3 +6,16 @@ class RobotsData(BaseModel):
     namespace: str
     robot_type: str
     robot_name: str
+
+
+class PosturesData(BaseMOdel):
+    pos_id: str
+    pos_name: str
+    j1: float
+    j2: float
+    j3: float
+    j4: float
+    j5: float
+    j6: float
+    angle: float
+    gap: int
