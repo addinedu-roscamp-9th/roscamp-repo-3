74996284a522562352bacdf48ff027dc2daff131
김찬희 <blackeagle10@icloud.com>
2026-02-05 16:33:55 +0900

    update(server): return modified pos to jetcobot

--- TechnicalResearch/server/app/services/jetcobot_service.py
+++ TechnicalResearch/server/app/services/jetcobot_service.py
@@ -3,21 +3,22 @@ from app.models.robots_model import RobotsData
 
 
 def test_connection(data: RobotsData):
-    print('\n=== Data from Jetcobot ===\n')
+    print("\n=== Data from Jetcobot ===\n")
     print(f"robot_id:   {data.robot_id}")
     print(f"namespace:  {data.namespace}")
     print(f"robot_type: {data.robot_type}")
     print(f"robot_name: {data.robot_name}")
     print()
 
+    # TODO: query from DB
     pos = Postures(
-        pos_id="foo",
-        pos_name="bar",
-        j1=0,
-        j2=0,
-        j3=0,
-        j4=0,
-        j5=0,
+        pos_id="p260205003",
+        pos_name="trash_side",
+        j1=-90,
+        j2=40,
+        j3=-90,
+        j4=5,
+        j5=4,
         j6=135,  # camera to bottom
         gap=0,  # completely open
     )
