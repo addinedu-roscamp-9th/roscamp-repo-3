668f7afa472f735c6fe3bdb839421d9bc720b019
김찬희 <blackeagle10@icloud.com>
2026-02-05 15:45:01 +0900

    remove(server): websocket

--- TechnicalResearch/server/main.py
+++ TechnicalResearch/server/main.py
@@ -2,7 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.routers import gui, jetcobot, pinky
-from app.websocket import websocket
 
 HOST = "0.0.0.0"
 PORT = 8000
@@ -13,7 +12,6 @@ app = FastAPI(title="Roboto Server", redirect_slashes=False)
 app.include_router(jetcobot.router, prefix="/jetcobot", tags=["jetcobot"])
 app.include_router(pinky.router, prefix="/pinky", tags=["pinky"])
 app.include_router(gui.router, prefix="/gui", tags=["gui"])
-app.include_router(websocket.router, prefix="/ws", tags=["websocket"])
 
 
 if __name__ == "__main__":
