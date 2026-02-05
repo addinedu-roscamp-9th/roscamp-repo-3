2eeab0c5f98ed6f38dd080384cfb57f2f530372e
김찬희 <blackeagle10@icloud.com>
2026-02-05 11:11:11 +0900

    change(server) model posture joints type to int

--- TechnicalResearch/server/app/models/postures_model.py
+++ TechnicalResearch/server/app/models/postures_model.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel
 class Postures(BaseModel):
     pos_id: str
     pos_name: str
-    j1: float
-    j2: float
-    j3: float
-    j4: float
-    j5: float
-    j6: float
+    j1: int
+    j2: int
+    j3: int
+    j4: int
+    j5: int
+    j6: int
     angle: int
     gap: int
