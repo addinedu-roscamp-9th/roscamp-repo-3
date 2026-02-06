2d4a8b0d78f5318b328f11603dde9c663d389f55
KimChanHee <blackeagle10@icloud.com>
2026-02-06 21:58:26 +0900

    qol(server): format and remove unnecessary stuffs from run, main

--- TechnicalResearch/server/run.py
+++ TechnicalResearch/server/run.py
@@ -1,26 +1,13 @@
-"""
-run.py
-
-FastAPI 서버 실행 전용 파일
-- 외부(ROS2 로봇)에서 접근 가능하도록 서버를 실행한다
-- app/main.py에 정의된 FastAPI 앱을 불러와 실행한다
-"""
-
 import uvicorn
+
+HOST = "0.0.0.0"  # 외부에서 연결 가능
+PORT = 8000
+DEBUG = False  # ROS 통신 안정성을 위해 비활성화
 
 if __name__ == "__main__":
     uvicorn.run(
-        # FastAPI 앱 위치
-        # app/main.py 안의 app 객체
-        "app.main:app",
-
-        # 외부 접속 허용 (라즈베리파이, 젯코봇 등)
-        host="0.0.0.0",
-
-        # 서버 포트
-        port=8000,
-
-        # ROS 통신 안정성을 위해 reload 비활성화
-        reload=False
+        "app.main:app",  # app/main.py 호출
+        host=HOST,
+        port=PORT,
+        reload=DEBUG,
     )
-
