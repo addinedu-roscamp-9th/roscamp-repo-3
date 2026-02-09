0f7f118192e50ea92e30cb650db2c28302d5ac1b
김찬희 <blackeagle10@icloud.com>
2026-02-09 17:16:41 +0900

    update(server): routers/jetcobot into http post connection

--- TechnicalResearch/server/app/routers/jetcobot.py
+++ TechnicalResearch/server/app/routers/jetcobot.py
@@ -1,25 +1,12 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from pydantic import ValidationError
+from fastapi import APIRouter
 
-from app.models.robots_model import RobotsData
 from app.services import jetcobot_service
 
 router = APIRouter()
 
 
-# ws://192.168.0.56:8000/jetcobot
-@router.websocket("")
-async def jetcobot_connection_test(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        while True:
-            raw = await websocket.receive_json()
-            data = RobotsData(**raw)
-            result = jetcobot_service.test_connection(data)
-            await websocket.send_json(result)
-    except WebSocketDisconnect:
-        print("Client disconnected normally")
-    except ValidationError as e:
-        print(f"Invalid data format from client: {e}")
-    except ValueError as e:
-        print(f"Service error: {e}")
+# POST http://192.168.0.56:8000/jetcobot
+@router.post("")
+def jetcobot_connection():
+    result = jetcobot_service.jetcobot_connection()
+    return result
--- TechnicalResearch/server/app/services/jetcobot_service.py
+++ TechnicalResearch/server/app/services/jetcobot_service.py
@@ -1,16 +1,8 @@
 from app.database import jetcobot_mapper
 from app.models.postures_model import Postures
-from app.models.robots_model import RobotsData
 
 
-def test_connection(data: RobotsData):
-    print("\n=== Data from Jetcobot ===\n")
-    print(f"robot_id:   {data.robot_id}")
-    print(f"namespace:  {data.namespace}")
-    print(f"robot_type: {data.robot_type}")
-    print(f"robot_name: {data.robot_name}")
-    print()
-
+def jetcobot_connection():
     poses = jetcobot_mapper.select_all_pos()
 
     # Convert ORM models to Pydantic models and return as list of dicts
