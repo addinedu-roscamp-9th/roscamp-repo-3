6a5bf1bff4ee07b0da3513bc348cb7c36f91fd3d
김찬희 <blackeagle10@icloud.com>
2026-02-11 13:46:37 +0900

    add(server): get_schedule() at main.py
    remove(server): printing queried schedules

--- TechnicalResearch/server/app/main.py
+++ TechnicalResearch/server/app/main.py
@@ -12,32 +12,19 @@ app.include_router(jetcobot.router, prefix="/jetcobot", tags=["jetcobot"])
 app.include_router(pinky.router, prefix="/pinky", tags=["pinky"])
 app.include_router(gui.router, prefix="/gui", tags=["gui"])
 
+schedules = []
+
 
 @app.on_event("startup")
 async def select_schedules():
+    global schedules
     try:
         schedules = schedule_mapper.select_all_schedules()
-
-        # TODO: execute the order when time comes
-
-        print("=" * 30)
-        print("query schedules")
-
-        if not schedules:
-            print("There is no schedule")
-            return
-
-        for schedule in schedules:
-            print(f"schedule_id: {schedule.schedule_id}")
-            print(f"cmd_id: {schedule.cmd_id}")
-            print(f"item_id: {schedule.item_id}")
-            print(f"position_id: {schedule.position_id}")
-            print(f"execute_time: {schedule.execute_time}")
-            print(f"cycle: {schedule.cycle}")
-            print(f"on_weekends: {schedule.on_weekends}")
-
-        print("=" * 30)
     except OperationalError as e:
         print(f"\nDatabase connection error: {e}")
     except SQLAlchemyError as e:
         print(f"\nDatabase error: {e}")
+
+
+def get_schedule():
+    return schedules
