fedbdcd0ef5dbddb0982d76ec695ce506d2e088a
KimChanHee <blackeagle10@icloud.com>
2026-02-15 17:06:15 +0900

    add(db): insert angles to angles table
    rename(db): table -> postures angles
    
    change(db): item_id fk to angles table
    
    add(db): items to items table
    
    chore(db): format positions table

--- TechnicalResearch/database/home_ai.sql
+++ TechnicalResearch/database/home_ai.sql
@@ -12,7 +12,7 @@ drop table if exists detections;
 drop table if exists items;
 drop table if exists commands;
 drop table if exists positions;
-drop table if exists postures;
+drop table if exists angles;
 
 create table users (
         user_id   varchar(30) primary key,
@@ -56,7 +56,11 @@ create table items (
         frequency int default 0
     );
 
-insert into items values ('i2602070001', 'choco stick', 10, 3);
+insert into items
+values
+  ('i2602150001', 'choco', 10, 3),
+  ('i2602150002', 'juice', 10, 3),
+  ('i2602150003', 'pill', 10, 3);
 
 create table commands (
         cmd_id   varchar(11) primary key,
@@ -71,16 +75,17 @@ create table positions (
         position_name varchar(30) not null,
         x             float not null,
         y             float not null,
-        w         float not null
+        w             float not null
     );
 
+insert into positions values ('p2602150001', 'charger', 0.0, 0.0, 0.0);
 insert into positions values ('p2602070001', 'drop zone', 0.2269, 0.2037, 0.7076);
-insert into positions values ('p2602100001', 'charger', 0.0, 0.0, 0.0);
 insert into positions values ('p2602100002', 'living room', 4.0, 5.0, 6.0);
 insert into positions values ('p2602100003', 'bed room', 7.0, 8.0, 9.0);
 
-create table postures (
+create table angles (
         pos_id   varchar(11) primary key,
+        item_id  varchar(11),
         pos_name varchar(30) not null,
         j1       float not null,
         j2       float not null,
@@ -88,15 +93,22 @@ create table postures (
         j4       float not null,
         j5       float not null,
         j6       float not null,
-        gap      int not null
+        foreign key (item_id) references items (item_id)
     );
 
--- "p" + 6 digit date + 4 digit int
-insert into postures
+-- "a" + 6 digit date + 4 digit int
+insert into angles
 values
-  ( "p2602050001", "pinky_side", 0, 40, -90, 5, 4, 135, 0 ),
-  ( "p2602050002", "shelve_side", 90, 40, -90, 5, 4, 135, 0 ),
-  ( "p2602050003", "trash_side", -90, 40, -90, 5, 4, 135, 0 );
+  ('a2602150001', null, 'home', 0, 0, 0, 0, 0, -47.33),
+  ('a2602150002', null, 'shelve_side', 47.8, 53.6, 323.9, -147.83, -3.83, 48.32),
+  ('a2602150003', 'i2602150001', 'pick_choco', -13.8, 273.3, 119.2, -170.71, 17.92, 48.2),
+  ('a2602150004', 'i2602150002', 'pick_juice', 71.4, 251.7, 136.4, -169.78, 10.65, 44.31),
+  ('a2602150005', 'i2602150003', 'pick_pill', -116.9, 244.0, 112.8, -175.41, 10.75, 46.85),
+  ('a2602150006', null, 'pinky_side', 115.4, -57.3, 301.2, -158.52, 4.56, -43.29),
+  ('a2602150007', null, 'drop', 260.8, -17.2, 43.9, -170.5, 22.21, -37.29),
+  ('a2602150008', null, 'trash_side', -49.4, -107.1, 336.3, -145.59, -12.03, -132.05),
+  ('a2602150009', null, 'trash_general', 38.3, -262.2, 267.2, -123.03, -22.08, -131.31),
+  ('a2602150010', null, 'trash_paper', -39.6, -272.2, 239.3, -138.81, -14.89, -143.59);
 
 create table schedules (
         schedule_id  varchar(11) primary key,
@@ -111,7 +123,7 @@ create table schedules (
         foreign key (position_id) references positions (position_id)
     );
 
-insert into schedules values ('s2602070001', 'c2602070001', 'i2602070001', 'p2602070001', '15:40:00', 1, True);
+insert into schedules values ('s2602070001', 'c2602070001', 'i2602150003', 'p2602070001', '15:40:00', 1, True);
 
 create table history (
         history_id     varchar(11) primary key,
